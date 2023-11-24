649a1dc671a0f117a5166795b419f983276c1a72
Prashant Laxmikant <pl@vmware.com>
2023-11-24 08:47:30 +0530

    package objects

---
+++ src/main/scala/packageobject/package.scala
@@ -0,0 +1,21 @@
+package object packageobject {
+
+  // field
+  val MAGIC_NUM = 42
+
+  // method
+  def echo(a: Any) {
+    println(a)
+  }
+
+  // enumeration
+  object Margin extends Enumeration {
+    type Margin = Value
+    val TOP, BOTTOM, LEFT, RIGHT = Value
+  }
+
+  // type definition
+  type MutableMap[K, V] = scala.collection.mutable.Map[K, V]
+  val MutableMap = scala.collection.mutable.Map
+  
+}
